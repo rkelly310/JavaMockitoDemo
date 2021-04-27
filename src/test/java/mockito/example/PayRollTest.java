0d231ae7e908f49f650b71a3c06e81b1e128315e
rkelly310 <77735205+rkelly310@users.noreply.github.com>
2021-04-27 15:15:54 -0400

    pushed to Github

--- src/test/java/mockito/example/PayRollTest.java
+++ src/test/java/mockito/example/PayRollTest.java
@@ -1,7 +1,11 @@
 package mockito.example;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
